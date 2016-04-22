f161f5617aff33613c44c423afde286eb23cf05b
spinclick <spinclick@users.noreply.github.com>
2016-04-22 19:01:48 -0400

    Cleanup and add more options

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 To my astonishment, there doesn't seem to be a port of the infamous
 [beep](http://github.com/johnath/beep/) command for the \*BSDs.
 
-This is a beep implementation using `wscons(4)`, meaning it should work under
-OpenBSD as well as NetBSD. It aims to be 100% compatible with the original beep
-command, though it is incomplete. However, it is good enough to [play the
-piano](https://github.com/watsyurname529/linuxbeeppiano).
+This is a re-implementation of the beep command using `wscons(4)`, meaning it
+should work under OpenBSD as well as NetBSD. It aims to be 100% compatible with
+the original, so if you find any behavioral differences, please open an issue
+or submit a pull request.
 
-Currently, the -h, -v, -s, -c, and long options are not implemented.
+All options from the original, both short and long, have been implemented. This
+means you can [play the piano](https://github.com/watsyurname529/linuxbeeppiano).
+
+License is 3-clause BSD.
--- main.c
+++ main.c
@@ -1,36 +1,84 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <stdlib.h>
+/* A simple implementation of the beep command using wscons */
+
 #include <stdio.h>
-#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <getopt.h>
 
 #include <sys/ioctl.h>
 #include <dev/wscons/wsconsio.h>
 
-// Default beep configuration
-#define FREQUENCY 400
-#define DURATION 200
-#define DELAY 0
-#define REPITITIONS 1
-#define DELAY_LAST (-1)
+#define VERSION_STRING "beep_for_bsd 0.1\n"
 
+// Default beep configuration
+#define FREQUENCY 400     // 400ms
+#define DURATION 200      // 200ms
+#define DELAY 0           // no delay between beeps
+#define REPITITIONS 1     // beep once
+#define DELAY_LAST (-1)   // no
+#define INPUT_STREAM (-1) // no
+
+// Different input modes
+#define INPUT_STREAM_CHAR 0
+#define INPUT_STREAM_LINE 1
+
+// Console file descriptor
+int consolefd;
+
+// Beep configuration
 typedef struct beep_args_s {
   int freq_hz;
   long duration_ms;
   long delay_ms;
   int repititions;
   int delay_on_last_beep;
+  int input_stream;
 } beep_args_t;
 
+// Structure for getopt_long
+static struct option longopts[] =
+{
+    { "new", no_argument, NULL, 'n'},
+    { "version", no_argument, NULL, 'v'},
+    { "help", no_argument, NULL, 'h'}
+};
+
+// Close the console and exit
+void exit_cleanly(int code) {
+  fflush(stdout);
+  close(consolefd);
+  exit(code);
+}
+
+void print_help_exit(int code) {
+  printf("Usage:\n");
+  printf("beep [-f freq] [-l length] [-r reps] [-d delay] ");
+  printf("[-D delay] [-s] [-c]\n");
+  printf("beep [options...] [-n] [--new] [options...] ...\n");
+  printf("beep [-h] [--help]\n");
+  printf("beep [-v] [-V] [--version]\n");
+
+  exit_cleanly(code);
+}
+
+void print_version_exit(int code) {
+  printf(VERSION_STRING);
+  exit_cleanly(code);
+}
+
+// Set beep arguments to defaults
 void reset_args(beep_args_t *args) {
   args->freq_hz = FREQUENCY;
   args->duration_ms = DURATION;
   args->delay_ms = DELAY;
   args->repititions = REPITITIONS;
   args->delay_on_last_beep = DELAY_LAST;
+  args->input_stream = INPUT_STREAM;
 }
 
+// Actually do the beeping
 void beep(long duration_ms, int freq_hz, int consolefd) {
 
   struct wskbd_bell_data wsb;
@@ -45,28 +93,86 @@ void beep(long duration_ms, int freq_hz, int consolefd) {
   usleep(duration_ms*1000);
 }
 
-void beep_run(beep_args_t args, int consolefd) {
+// Send a short beep to stop the speaker, then exit
+void interrupt_handler(int signal) {
+  beep(0, 0, consolefd);
+  exit_cleanly(2);
+}
 
-  int r;
-  for (r=0;r<args.repititions;r++) {
+// Beep for every char on stdin
+void beep_on_char(beep_args_t *args, int consolefd) {
 
-    beep(args.duration_ms, args.freq_hz, consolefd);
+  while (1) {
 
-    if (args.delay_on_last_beep == -1 && r == args.repititions-1) 
+    char c = getchar();
+
+    if (feof(stdin))
       break;
 
-    usleep(args.delay_ms*1000);
+    putchar(c);
+    fflush(stdout);
+
+    beep(args->duration_ms, args->freq_hz, consolefd);
+    usleep(args->delay_ms*1000);
+
   }
 }
 
+// Beep for every line on stdin
+void beep_on_line(beep_args_t *args, int consolefd) {
+  
+  while (1) {
+
+    char c = getchar();
+
+    if (feof(stdin))
+      break;
+
+    putchar(c);
+    fflush(stdout);
+
+    if (c == '\n') {
+      beep(args->duration_ms, args->freq_hz, consolefd);
+      usleep(args->delay_ms*1000);
+    }
+  }
+}
+
+// Decide how to beep based on config
+void beep_run(beep_args_t *args, int consolefd) {
+
+  // -s
+  if (args->input_stream == INPUT_STREAM_LINE) {
+    beep_on_line(args, consolefd);
+    return;
+  }
+
+  // -c
+  if (args->input_stream == INPUT_STREAM_CHAR) {
+    beep_on_char(args, consolefd);
+    return;
+  }
+
+  // Everything else
+  int r;
+  for (r=0;r<args->repititions;r++) {
+
+    beep(args->duration_ms, args->freq_hz, consolefd);
+
+    if (args->delay_on_last_beep == -1 && r == args->repititions-1) 
+      break;
+
+    usleep(args->delay_ms*1000);
+  }
+}
+
+// Process commandline, return the number of arguments left
 int get_beep_args(int argc, char *argv[], beep_args_t *args) {
 
   int args_left = argc;
 
   char ch;
-  while ((ch = getopt(argc, argv, "e:f:l:r:d:D:nschv")) != -1) {
-
-    //printf("argc: %d\n", argc);
+  while ((ch = getopt_long(argc, argv, "e:f:l:r:d:D:nschvV", longopts, NULL)) != -1) {
 
     switch ((char)ch) {
       case 'e': printf("e: %s\n", optarg); break;
@@ -89,10 +195,14 @@ int get_beep_args(int argc, char *argv[], beep_args_t *args) {
       case 'n':  
         return args_left;
       break;
-      case 's': printf("s\n"); break;
-      case 'c': printf("c\n"); break;
-      case 'h': printf("h\n"); break;
-      case 'v': printf("v\n"); break;
+      case 's': args->input_stream = INPUT_STREAM_LINE; 
+      break;
+      case 'c': args->input_stream = INPUT_STREAM_CHAR; 
+      break;
+      case 'h': print_help_exit(0); break;
+      case 'v': print_version_exit(0); break;
+      case 'V': print_version_exit(0); break;
+      case '?': print_help_exit(1); break;
     }
     args_left -= 1;
   }
@@ -102,16 +212,18 @@ int get_beep_args(int argc, char *argv[], beep_args_t *args) {
 int main(int argc, char *argv[]) {
 
   beep_args_t args;
-
   reset_args(&args);
-
-  int consolefd = open("/dev/console", O_WRONLY);
-
   int args_left = argc;
+
+  // Stop beeping on process termination 
+  signal(SIGINT, interrupt_handler);
+  signal(SIGTERM, interrupt_handler);
+
+  consolefd = open("/dev/console", O_WRONLY);
 
   while (args_left > 0) {
     args_left = get_beep_args(argc, argv, &args);
-    beep_run(args, consolefd);
+    beep_run(&args, consolefd);
     reset_args(&args);
   }
 
